bb550a9b8ea7f930305b735104a7de14f7f71283
Mostafa-K-Ramadan <mostafa1115@hotmail.com>
2022-03-19 19:03:37 +0300

    Task, Get By Id method

---
+++ Application/Task/GetById.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.DTOs;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Task
+{
+    public class GetById
+    {
+         public class Query : IRequest<TaskDTO> { 
+
+            public int Id {get; set;} 
+        }
+
+        public class Handler : IRequestHandler<Query, TaskDTO>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _mapper = mapper;
+                _context = context;
+            }
+
+            public async Task<TaskDTO> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var task = await _context.Tasks
+                    .ProjectTo<TaskDTO>(_mapper.ConfigurationProvider)
+                    .FirstOrDefaultAsync(x => x.TaskId == request.Id);
+
+                return task;
+            }
+        }
+    }
+}
